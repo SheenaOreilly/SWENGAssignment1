3e692a2d4884aefde672dd151c304b181e4195eb
SheenaOreilly <oreils21@tcd.ie>
2022-10-04 13:07:21 +0100

    adding multiplication code and tests to java class

---
+++ SWENG/src/CalculatorTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CalculatorTest
+{
+    @Test
+    public void testConstructor()
+    {
+        new calculator();
+    }
+
+    @Test
+    public void testMultiplication()
+    {
+        assertEquals(calculator.multiplication(1,2), 2);
+        assertEquals(calculator.multiplication(5,6), 30);
+    }
+}
---
+++ SWENG/src/calculator.java
@@ -0,0 +1,10 @@
+public class calculator
+{
+
+    public static int multiplication(int x, int y)
+    {
+        int answer  = x * y;
+        return answer;
+    }
+
+}
